cb23f0b7207a39dcd7cbc2fee6956e532be58dcf
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-05-27 17:46:51 -0400

    Update JS function to display fun facts in table

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,16 +16,41 @@
  * Adds a random greeting to the page.
  */
 function addRandomFunFact() {
-  const greetings =
+  const funfacts =
       ['I love hiking, biking, and spending time outdoors.', 
       'At Columbia, I am part of the Pops Orchestra, an ensemble dedicated to arranging and playing pop pieces for orchestra.',
        'I have two pet cats.',
-        'My favorite movie is The Matrix.'];
+        'My favorite movie is The Matrix.', 
+        'I am a middle child - I have an older brother and two younger sisters.', 
+        'My favorite food is garbanzo beans.', 
+        'Currently, I am attempting to grow potatoes outside.',
+        'My favorite animal is a panda.'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const funfact = funfacts[Math.floor(Math.random() * funfacts.length)];
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  // Add it to the page in a talbe.
+  var funFactTable = document.getElementById('funfact-table');
+  const funFactRowText = '<tr><td>' + funfact + '</td></tr>';
+
+  if(!tableText.includes(funFactRowText)){
+        tableText += '<tr><td>' + funfact + '</td></tr>';
+        countTableEntries++;
+        document.getElementById('funfact-message').innerHTML = "";
+  }else if(countTableEntries < funfacts.length){
+      document.getElementById('funfact-message').innerHTML = "Repeat fact - try again!";
+  }else{
+      //need to deal with edge case that the table has all the entries more elegantly
+      document.getElementById('funfact-message').innerHTML = "Congrats - you've gone through all of the fun facts!";
+  }
+  
+  funFactTable.innerHTML = tableText + '</table>';
 }
+
+//outside of function because we want to add to it each time addRandomFunFact is called
+var tableText = `<table style="width:100%"> 
+            <tr> 
+                <th>Fun Fact</th> 
+            </tr>`;
+
+var countTableEntries = 0;
