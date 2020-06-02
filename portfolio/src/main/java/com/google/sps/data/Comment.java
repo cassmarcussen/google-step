fc55eb4d81a348bf8f8f721f1ea4a55586ce4437
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-02 21:18:03 +0000

    Add Comment java class for comments to page

---
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -0,0 +1,38 @@
+package com.google.sps.servlets;
+
+/*
+Class for a comment to add to the website.
+Contains name and message private fields.
+*/
+public class Comment {
+
+    private String name;
+    private String message;
+
+    public Comment(){
+        name = "";
+        message = "";
+    }
+
+    public Comment(String newName, String newComment){
+        name = newName;
+        message = newComment;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String newMessage){
+        message = newMessage;
+    }
+
+}
