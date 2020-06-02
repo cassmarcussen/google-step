83fe3f748ac68233f0c6b32dc9b4e041cdd0ad4e
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-02 16:10:51 +0000

    Implement getNumComments

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -31,7 +31,7 @@ import java.util.*;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/comments")
 public class DataServlet extends HttpServlet {
-    
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
@@ -42,7 +42,20 @@ public class DataServlet extends HttpServlet {
     PreparedQuery results = datastore.prepare(query);
 
     List<String> messages = new ArrayList<>();
+
+    /*globalNumComments = getNumComments(request);
+    System.err.println("globalNumComments: " + globalNumComments);*/
+
+    //globalNumComments = 3;
+
+    int numIterations = 0;
+
     for (Entity entity : results.asIterable()) {
+      if(numIterations >= globalNumComments){
+          break;
+      }
+      numIterations++;
+
       String message = (String) entity.getProperty("message");
       
       messages.add(message);
@@ -60,15 +73,49 @@ public class DataServlet extends HttpServlet {
 
     String newComment = request.getParameter("text-input");
 
-    Entity taskEntity = new Entity("Comment");
-    taskEntity.setProperty("message", newComment);
+    if(newComment != null){
 
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.put(taskEntity);
+        Entity taskEntity = new Entity("Comment");
+        taskEntity.setProperty("message", newComment);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(taskEntity);
+
+    }
+
+    globalNumComments = getNumComments(request);
+    System.out.println("globalNumComments: " + globalNumComments);
 
     // Redirect back to the HTML page.
     response.sendRedirect("/step_projects.html");
 
   }
+
+
+  /** Returns the choice entered by the player, or -1 if the choice was invalid. */
+  private int getNumComments(HttpServletRequest request) {
+
+    String numCommentsString = request.getParameter("num-comments");
+
+    // Convert the input to an int.
+    int numComments = 10;
+    try {
+      numComments = Integer.parseInt(numCommentsString);
+    } catch (NumberFormatException e) {
+      System.err.println("Could not convert to int: " + numCommentsString);
+      numComments = 10;
+    }
+
+    // Check that the input is between 1 and 3.
+    if (numComments < 0) {
+      System.err.println("Number of comments requested is out of range (must be nonnegative): " + numComments);
+      numComments = 10;
+    }
+
+    return numComments;
+  }
+
+  int globalNumComments = 10;
   
 }
+
