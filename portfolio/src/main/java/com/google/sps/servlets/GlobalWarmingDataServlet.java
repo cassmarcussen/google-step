b67c046f4f6ba1e6f956b75e3bbcdce4c34919a8
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-08 19:57:30 +0000

    Add Servlet to handle global warming data

---
+++ portfolio/src/main/java/com/google/sps/servlets/GlobalWarmingDataServlet.java
@@ -0,0 +1,52 @@
+package com.google.sps.servlets;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Returns global warming sentiment data as a JSON object, e.g. {"2017": 52, "2018": 34}] */
+@WebServlet("/global-warming-sentiment-data")
+public class GlobalWarmingDataServlet extends HttpServlet {
+
+  private LinkedHashMap<String, Integer> globalWarmingSentiment = new LinkedHashMap<>();
+
+  @Override
+  public void init() {
+    Scanner scanner = new Scanner(getServletContext().getResourceAsStream(
+        "/WEB-INF/global-warming-sentiment-data.csv"));
+    
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] cells = line.split(",");
+
+      String tweet = (String) cells[0];
+      Integer existence = 0;
+      if(((String) cells[1]).equals("Yes")){
+          existence = 1;
+      }else if (((String) cells[1]).equals("No")){
+          existence = 0;
+      }else{
+          //Go to the next iteration of the while loop, because we don't want to add in values such as N/A.
+          continue;
+      }
+
+     // String existence_confidence = (String) cells[2];
+
+      globalWarmingSentiment.put(tweet, existence);
+    }
+    scanner.close();
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    Gson gson = new Gson();
+    String json = gson.toJson(globalWarmingSentiment);
+    response.getWriter().println(json);
+  }
+}
