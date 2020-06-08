16b08f15013b89a962d71432e5461b082ac49a48
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-08 19:58:21 +0000

    Add function to draw global warming chart

--- portfolio/src/main/webapp/charts-script.js
+++ portfolio/src/main/webapp/charts-script.js
@@ -1,5 +1,14 @@
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawPieChart);
+google.charts.setOnLoadCallback(drawCharts);
+
+/* drawCharts draws all of the charts to display. It is called in setOnLoadCallback. 
+The reasoning behind drawCharts is that setOnLoadCallback executes one function on load,
+ and drawCharts provides a centralized place for which to load all of the charts on the website.
+*/
+function drawCharts(){
+    //drawPieChart();
+    drawGlobalWarmingChart();
+}
 
 /** Creates a chart and adds it to the page. */
 function drawPieChart() {
@@ -22,4 +31,29 @@ function drawPieChart() {
   const chart = new google.visualization.PieChart(
       document.getElementById('pie-chart-container'));
   chart.draw(data, options);
+}
+
+/** Fetches bigfoot sightings data and uses it to create a chart. */
+function drawGlobalWarmingChart() {
+  fetch('/global-warming-sentiment-data').then(response => response.json())
+  .then((globalWarmingSentiment) => {
+    const data = new google.visualization.DataTable();
+    //have Sentiment and frequency/amount of occurrence
+    data.addColumn('string', 'Tweet');
+    data.addColumn('number', 'Sentiment');
+    Object.keys(globalWarmingSentiment).forEach((tweet) => {
+      data.addRow([tweet, globalWarmingSentiment[tweet]]);
+    });
+
+    const options = {
+      'title': 'Global Warming Sentiment',
+      'width':600,
+      'height':500,
+       histogram: { bucketSize: 1 }
+    };
+
+    const chart = new google.visualization.Histogram(
+        document.getElementById('globalwarming-chart-container'));
+    chart.draw(data, options);
+  });
 }
