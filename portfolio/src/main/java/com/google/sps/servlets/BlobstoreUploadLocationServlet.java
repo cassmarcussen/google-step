dd3b28bcee1d638cbf256669e6dcaa6a3c5a2b90
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-10 21:27:59 +0000

    Blobstore get request

---
+++ portfolio/src/main/java/com/google/sps/servlets/BlobstoreUploadLocationServlet.java
@@ -0,0 +1,58 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+
+/**
+ * When the fetch() function requests the /blobstore-upload-url URL, the content of the response is
+ * the URL that allows a user to upload a file to Blobstore. If this sounds confusing, try running a
+ * dev server and navigating to /blobstore-upload-url to see the Blobstore URL.
+ */
+@WebServlet("/blobstore-upload-location-url")
+public class BlobstoreUploadLocationServlet extends HttpServlet {
+
+  //called whenever Locations loads (in its body load)
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    System.out.println("doGet");
+    Query query = new Query("LocationFiles");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    List<LocationImg> locationImgs = new ArrayList<LocationImg>();
+
+    for (Entity entity : results.asIterable()) {
+      
+      String url = (String) entity.getProperty("imageUrl");
+      String message = (String) entity.getProperty("message");
+      long id = entity.getKey().getId();
+
+      LocationImg img = new LocationImg(url, message, id);
+
+      locationImgs.add(img);
+
+    
+    }
+
+    Gson gson = new Gson();
+
+    response.setContentType("application/json;");
+
+    response.getWriter().println(gson.toJson(locationImgs));
+    
+  }
+}
